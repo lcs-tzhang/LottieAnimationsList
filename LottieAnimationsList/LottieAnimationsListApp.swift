e88d0fef037659fe22344002d6a55d1b29b6479b
Tiffany Zhang <tzhang@lcs.on.ca>
2025-02-07 12:02:24 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by xinyu zhang on 2025-02-07.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
